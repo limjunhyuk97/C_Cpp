abdd73dce061566a0960339ddbbbcb3d482d7c92
Jun Hyuk Lim <59442344+limjunhyuk97@users.noreply.github.com>
2020-12-06 23:53:51 +0900

    Update bankingSystem.cpp
    구현은 완료했다.
    
    upgrade 해보자.
    - id 입력을 숫자가 아닌 다른 수를 넣었을 때 무한loop를 돌게 됨 -> 잘못된 input를 커버할 수 있게 수정해보기
    - hash collision 방지를 hash bucket으로 해보려고 함. -> 고정 배열 말고, 가변 배열로 2차원 배열을 cpp에서 구현하는 방법 사용해보기 
    - 비슷한 역할을 하는 코드 부분들을 하나의 함수로 엮어서 기능을 분화시키기 (의존도를 떨어뜨리기)

--- week1/bankingSystem.cpp
+++ week1/bankingSystem.cpp
@@ -2,14 +2,23 @@
 #include <stdio.h>
 
 using namespace std;
+
 inline void message();
 int branch(int num);
-void makeAccount(void);
+void makeAccount();
 void deposit(void);
 void withdraw(void);
 void printAllAccount(void);
 void exitProgram(void);
 void errmsg(void);
+int hashFunction(int num);
+
+struct account {
+	int id = 0;
+	char name[15] = "";
+	int reserve = 0;
+};
+account member[50][50];
 
 inline void message() {
 	cout << "-----Menu-----" << endl;
@@ -18,61 +27,130 @@ inline void message() {
 	cout << "3. 출금" << endl;
 	cout << "4. 계좌정보 전체 출력" << endl;
 	cout << "5. 프로그램 종료" << endl;
-	cout << "선택 : " << endl;
+	cout << "선택 : ";
 }
+
 int branch(int num) {
 	if (num == 1) {
 		makeAccount();
-		return 0;
+		return 1;
 	}
 	else if (num == 2) {
 		deposit();
-		return 0;
+		return 1;
 	}
 	else if (num == 3) {
 		withdraw();
-		return 0;
+		return 1;
 	}
 	else if (num == 4) {
 		printAllAccount();
-		return 0;
+		return 1;
 	}
 	else if (num == 5) {
 		exitProgram();
-		return 1;
+		return 0;
 	}
 	else {
 		errmsg();
-		return 0;
+		return 1;
 	}
 }
-void makeAccount(void) {
 
+void makeAccount() {
+	int id = 0;
+	int cnt = 0;
+	int hashValue = 0;
+	cout << "\n[계좌개설]\n";
+	cout << "계좌 ID : ";
+	cin >> id;
+	hashValue = hashFunction(id);
+	while (1) {
+		if (member[hashValue][cnt].id == 0) {
+			member[hashValue][cnt].id = id;
+			break;
+		}
+		else ++cnt;
+	}
+	cout << "이름 : ";
+	cin >> member[hashValue][cnt].name;
+	cout << "입금액 : ";
+	cin >> member[hashValue][cnt].reserve;
+	cout << "입금완료 / 잔액 : " << member[hashValue][cnt].reserve << endl << endl;
 }
+
 void deposit(void) {
-
+	int findID;
+	int hashValue;
+	int cnt = 0, tmp = 0;
+	cout << "\n계좌 ID : ";
+	cin >> findID;
+	hashValue = hashFunction(findID);
+	while (1) {
+		if (member[hashValue][cnt].id == findID) break;
+		else ++cnt;
+	}
+	cout << "입금액 : ";
+	cin >> tmp;
+	member[hashValue][cnt].reserve += tmp;
+	cout << "입금완료 / 잔액 : " << member[hashValue][cnt].reserve << endl << endl;
 }
+
 void withdraw(void) {
-
+	int findID;
+	int hashValue;
+	int cnt = 0, tmp = 0;
+	cout << "\n계좌 ID : ";
+	cin >> findID;
+	hashValue = hashFunction(findID);
+	while (1) {
+		if (member[hashValue][cnt].id == findID) break;
+		else ++cnt;
+	}
+	cout << "출금액 : ";
+	cin >> tmp;
+	member[hashValue][cnt].reserve -= tmp;
+	cout << "입금완료 / 잔액 : " << member[hashValue][cnt].reserve << endl << endl;
 }
+
 void printAllAccount(void) {
-
+	int cnt = 0;
+	for (int i = 0; i < 50; ++i) {
+		for (int j = 0; j < 50; ++j) {
+			if (member[i][j].id != 0) {
+				++cnt;
+				cout << "\n==============================" << "\n이름 : " << member[i][j].name;
+				cout << "\n입금액 : " << member[i][j].reserve;
+				cout << "\n입금완료 / 잔액 : " << member[i][j].reserve << endl << "==============================" << endl;
+			}
+			else continue;
+		}
+	}
+	if (cnt == 0) cout << "\n개설된 계좌가 없습니다!\n\n";
+	else cout << endl;
 }
+
 void exitProgram(void) {
-
+	cout << "\n감사합니다. 프로그램을 종료합니다.\n\n";
 }
+
 void errmsg(void) {
-
+	cout << "\nWrond input number. try again.\n\n";
 }
 
+int hashFunction(int num) {
+	return num % 50;
+}
 
 int main(void) {
 
-	while (1) {
+	int flag = 1;
+
+	while (flag) {
 		message();
 		int choose = 0;
 		cin >> choose;
-		branch(choose);
+		flag = branch(choose);
 	}
 
 	return 0;
