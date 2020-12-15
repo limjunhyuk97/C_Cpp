81feafc4e2cc7e8dffdbbee8984bab65a0074cf3
Jun Hyuk Lim <59442344+limjunhyuk97@users.noreply.github.com>
2020-12-15 11:45:44 +0900

    Update quiz04_03_2.cpp

--- week3/quiz04_03_2.cpp
+++ week3/quiz04_03_2.cpp
@@ -1,23 +1,51 @@
 #include <iostream>
+#include <cstring>
+#include <string>
 using namespace std;
 
 namespace COMP_POS{
-	enum{ Clerk, Senior, Assist, Manager };
+	enum POS{ CLERK, SENIOR, ASSIST, MANAGER};
 
-
+	string PosToKor(POS value) {
+		if (value == CLERK) return "사원";
+		else if (value == SENIOR) return "주임";
+		else if (value == ASSIST) return "대리";
+		else if (value == MANAGER) return "과장";
+		else return "잘못된 입력";
+	}
 }
 
 class NameCard {
 private:
-	char* myName;
-	char* companyName;
-	char* callNumber;
-	// Clerk 사원, Senior 주임, Assist 대리, Manager 과장
+	string myName;
+	string myCompany;
+	string myNumber;
+	COMP_POS::POS position;
 
 public:
-	NameCard(char *name, char *companyName, char*call, )			//enum인자를 무엇으로 받아야 할까..?
-
+	NameCard(string name, string cName, string call, COMP_POS::POS pos) {
+		myName = name;
+		myCompany = cName;
+		myNumber = call;
+		position = pos;
+	}
+	void ShowNameCardInfo() {
+		cout << "이름 : " << myName << endl;
+		cout << "회사 : " << myCompany << endl;
+		cout << "전화번호 : " << myNumber << endl;
+		cout << "직급 : " << COMP_POS::PosToKor(position)<<endl<<endl;
+	}
 
 };
 
-// enum과 namespace 내용 복습 요망
+int main(void) {
+
+	NameCard manClerk("Lee", "ABCEng", "010-1111-2222", COMP_POS::CLERK);
+	NameCard manSenior("Hong", "OrangeEng", "010-3333-4444", COMP_POS::SENIOR);
+	NameCard manAssist("Kim", "SoGoodComp", "010-5555-6666", COMP_POS::ASSIST);
+	manClerk.ShowNameCardInfo();
+	manSenior.ShowNameCardInfo();
+	manAssist.ShowNameCardInfo();
+
+	return 0;
+}
