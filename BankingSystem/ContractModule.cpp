c03bef02bb8ec1c8d3648f446e357713ad2906c4
Jun Hyuk Lim <59442344+limjunhyuk97@users.noreply.github.com>
2020-12-18 12:13:38 +0900

    Update ContractModule.cpp

--- BankingSystem/ContractModule.cpp
+++ BankingSystem/ContractModule.cpp
@@ -5,16 +5,46 @@
 #include "CONTRACT_MODULE.h"
 #define ERRMSG_SYS "\nWrong input number. Try again.\n\n"
 #define ERRMSG_ID "\nWrong input Id. Try again.\n\n"
+#define ERRMSG_TY "\nWrong input type. Try again.\n"
 #define TERMSG "\nThank you. Terminating Program.\n\n"
 using namespace std;
 namespace CONTRACT_MODULE {
 
-	bool ErrFilterId(int id, int hashValue, std::vector < std::vector <CUSTOMER_INFO::Customer> > arr) {
-		if (CONTRACT_MODULE::IdIndexLocatingFunc(id, hashValue, arr) == 0) return false;
-		else return true;
-	}
+	namespace ERR_MODULE {
+		bool ErrFilterIdUnknown(int id, int hashValue, std::vector < std::vector <CUSTOMER_INFO::Customer> > arr) {
+			if (CONTRACT_MODULE::IdIndexLocatingFunc(id, hashValue, arr) == 0) return false;
+			else return true;
+		}
 
-	void ErrPrintId() { cout << ERRMSG_ID << endl << endl; return; }
+		int ErrFilterIdDataType() {
+			int id;
+			while (1) {
+				cin >> id;
+				if (cin.fail()) { cin.clear(); cin.ignore(256, '\n'); cout << ERRMSG_TY << endl << "계좌 ID : "; continue; }
+				else { cin.ignore();  return id; };
+			}
+		}
+
+		int ErrFilterDepoMoneyDataType() {
+			int money;
+			while (1) {
+				cin >> money;
+				if (cin.fail()) { cin.clear(); cin.ignore(256, '\n'); cout << ERRMSG_TY << endl << "입금액 : "; continue; }
+				else { cin.ignore(); return money; };
+			}
+		}
+
+		int ErrFilterWithdrawMoneyDataType() {
+			int money;
+			while (1) {
+				cin >> money;
+				if (cin.fail()) { cin.clear(); cin.ignore(256, '\n'); cout << ERRMSG_TY << endl << "출금액 : ";  continue; }
+				else { cin.ignore(); return money; };
+			}
+		}
+
+		void ErrPrintTy() {}
+	}
 
 	int IdIndexLocatingFunc(int id, int hashedId, std::vector < std::vector <CUSTOMER_INFO::Customer> >& container) {
 		int ans = 0;
@@ -40,13 +70,12 @@ namespace CONTRACT_MODULE {
 
 		cout << "\n[계좌개설]\n";
 		cout << "계좌 ID : ";
-		cin >> id;
+		id = ERR_MODULE::ErrFilterIdDataType();
 		hashValue = HashingFunc(id);
-		cout << "이름 : ";
-		tmp = cin.get();
+		cout << "성함 : ";
 		getline(cin, name);
 		cout << "입금액 : ";
-		cin >> money;
+		money = ERR_MODULE::ErrFilterDepoMoneyDataType();
 		cout << "***개설을 축하드립니다!!***" << endl;
 		cout << "입금완료 / 잔액 : " << money << endl << endl;
 		CUSTOMER_INFO::Customer client(id, name, money);
@@ -60,14 +89,14 @@ namespace CONTRACT_MODULE {
 
 		cout << "\n[계좌입금]\n";
 		cout << "계좌 ID : ";
-		cin >> id;
+		id = ERR_MODULE::ErrFilterIdDataType();
 		hashValue = HashingFunc(id);
-		if (!ErrFilterId(id, hashValue, arr)) {
-		ErrPrintId();
+		if (!ERR_MODULE::ErrFilterIdUnknown(id, hashValue, arr)) {
+			ERR_MODULE::ErrPrintId();
 			return;
 		}
 		cout << "입금액 : ";
-		cin >> money;
+		money = ERR_MODULE::ErrFilterDepoMoneyDataType();
 		CUSTOMER_INFO::Customer& exactLocation = arr[HashingFunc(id)][IdIndexLocatingFunc(id, HashingFunc(id), arr)];
 		exactLocation.ReserveIn(money);
 		cout << "입금완료 / 잔액 : " << exactLocation.ReserveReturn() << endl;
@@ -76,14 +105,14 @@ namespace CONTRACT_MODULE {
 	void Withdrawal::newWithdrawal(std::vector< std::vector<CUSTOMER_INFO::Customer> >& arr) {
 		cout << "\n[계좌출금]\n";
 		cout << "계좌 ID : ";
-		cin >> id;
+		id = ERR_MODULE::ErrFilterIdDataType();
 		hashValue = HashingFunc(id);
-		if (!ErrFilterId(id, hashValue, arr)) {
-			ErrPrintId();
+		if (!ERR_MODULE::ErrFilterIdUnknown(id, hashValue, arr)) {
+			ERR_MODULE::ErrPrintId();
 			return;
 		}
 		cout << "출금액 : ";
-		cin >> money;
+		money = ERR_MODULE::ErrFilterWithdrawMoneyDataType();
 		CUSTOMER_INFO::Customer& exactLocation = arr[HashingFunc(id)][IdIndexLocatingFunc(id, HashingFunc(id), arr)];
 		exactLocation.ReserveOut(money);
 		cout << "입금완료 / 잔액 : " << exactLocation.ReserveReturn() << endl;
