16ef404d52d51769fd2a897879b775dc130a1bb3
Jun Hyuk Lim <59442344+limjunhyuk97@users.noreply.github.com>
2020-12-28 18:07:44 +0900

    Update Customer.cpp

--- BankingSystem/Customer.cpp
+++ BankingSystem/Customer.cpp
@@ -4,25 +4,35 @@ namespace CUSTOMER_INFO{
 
 	Customer::Customer(int id, std::string name, int reserve)
 		: id(id), name(name), reserve(reserve) {}
+
 	Customer::Customer()
 		: id(), name(), reserve() {}
-	bool Customer::IdCheck(int input) {
+
+	Customer::Customer(const Customer& ref)
+		: id(ref.id), name(ref.name), reserve(ref.reserve) {}
+
+	bool Customer::IdCheck(int input) const {
 		if (input == id) return true;
 		else return false;
 	}
+
 	void Customer::ReserveIn(int in) {
 		reserve += in;
 	}
+
 	void Customer::ReserveOut(int out) {
 	reserve -= out;
 	}
-	int Customer::IdReturn() {
+
+	int Customer::IdReturn() const {
 		return id;
 	}
-	int Customer::ReserveReturn() {
+
+	int Customer::ReserveReturn() const {
 		return reserve;
 	}
-	std::string Customer::NameReturn() {
+
+	std::string Customer::NameReturn() const {
 		return name;
 	}
 
