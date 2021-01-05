609eab5d63fda80e060c7af39fc8049d7d19acee
Jun Hyuk Lim <59442344+limjunhyuk97@users.noreply.github.com>
2021-01-05 23:38:26 +0900

    Update ContractModule.h

--- BankingSystem/ContractModule.h
+++ BankingSystem/ContractModule.h
@@ -5,54 +5,56 @@
 #pragma once
 namespace CONTRACT_MODULE {
 
-	namespace ERR_MODULE {
-		bool ErrFilterIdUnknown(int id, int hashValue, std::vector < std::vector <CUSTOMER_INFO::Customer> > arr);
-
-		int ErrFilterIdDataType();
-
-		int ErrFilterDepoMoneyDataType();
-
-		int ErrFilterWithdrawMoneyDataType();
-
-		void ErrPrintId();
-	}
-
-	int IdIndexLocatingFunc(int id, int hashedId, std::vector < std::vector <CUSTOMER_INFO::Customer> >& container);
+	int IdIndexLocatingFunc(int id, int hashedId, std::vector < std::vector <CUSTOMER_INFO::Customer*> >& container);
 
 	int HashingFunc(int num);
 
+	namespace ERR_MODULE {
+		bool ErrFilterIdUnknown(int id, int hashValue, std::vector < std::vector <CUSTOMER_INFO::Customer *> > arr);
+
+		int ErrFilterIdDataType();
+
+		int ErrFilterMoneyDataType(int select);
+
+		void ErrPrintId();
+	}
+	
 	class Account {
 	private:
 		int id;
-		int hashValue;
+		int hashvalue;
 		int money;
+		int interest;
+		int creditrate;
 		std::string name;
 	public:
-		void newAccount(std::vector< std::vector<CUSTOMER_INFO::Customer> >& arr);
+		void newAccount(std::vector< std::vector<CUSTOMER_INFO::Customer*> >& arr);
+		void basicInput();
 	};
 
 	class Deposit {
 	private:
 		int id;
-		int hashValue;
+		int hashvalue;
 		int money;
 	public:
-		void newDeposit(std::vector< std::vector<CUSTOMER_INFO::Customer> >& arr);
+		void newDeposit(std::vector< std::vector<CUSTOMER_INFO::Customer *> >& arr);
 	};
 
 	class Withdrawal {
 	private:
 		int id;
-		int hashValue;
+		int hashvalue;
 		int money;
 	public:
-		void newWithdrawal(std::vector< std::vector<CUSTOMER_INFO::Customer> >& arr);
+		void newWithdrawal(std::vector< std::vector<CUSTOMER_INFO::Customer *> >& arr);
 	};
 
 	class PrintAllAccount {
 	public:
-		void NewPrintAllAccount(std::vector< std::vector<CUSTOMER_INFO::Customer> >& arr) const;
+		void NewPrintAllAccount(std::vector< std::vector<CUSTOMER_INFO::Customer *> >& arr) const;
 	};
+
 	class Branch {
 	private:
 		int select;
@@ -62,8 +64,8 @@ namespace CONTRACT_MODULE {
 		PrintAllAccount printIt;
 
 	public:
-		void ShowBranch(std::vector< std::vector <CUSTOMER_INFO::Customer> >& arr);
-		void SelectBranch(int n, std::vector< std::vector <CUSTOMER_INFO::Customer> >& arr);
+		void ShowBranch(std::vector< std::vector <CUSTOMER_INFO::Customer *> >& arr);
+		void SelectBranch(int n, std::vector< std::vector <CUSTOMER_INFO::Customer *> >& arr);
 		bool ExitLoop();
 	};
 
