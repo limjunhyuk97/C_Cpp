0b50b609ba68d8663098924844e84d4419c736ec
Jun Hyuk Lim <59442344+limjunhyuk97@users.noreply.github.com>
2021-01-05 23:38:52 +0900

    Update Customer.cpp

--- BankingSystem/Customer.cpp
+++ BankingSystem/Customer.cpp
@@ -1,4 +1,5 @@
 #include <string>
+#include <iostream>
 #include "CUSTOMER.h"
 namespace CUSTOMER_INFO{
 
@@ -20,20 +21,59 @@ namespace CUSTOMER_INFO{
 		reserve += in;
 	}
 
+	void Customer::ReserveInterest() {
+		reserve = (int)(reserve * ShowInterestRate());
+	}
+
 	void Customer::ReserveOut(int out) {
 	reserve -= out;
 	}
 
-	int Customer::IdReturn() const {
+	int Customer::GetId() const {
 		return id;
 	}
 
-	int Customer::ReserveReturn() const {
+	int Customer::GetReserve() const {
 		return reserve;
 	}
 
-	std::string Customer::NameReturn() const {
+	std::string Customer::GetName() const {
 		return name;
 	}
 
+	double NACustomer::ShowInterestRate() const {
+		return 1+(interest)/(double)100;
+	}
+
+	int NACustomer::GetInterest() const {
+		return interest;
+	}
+
+	void NACustomer::ShowAllInfo(int *cnt) const {
+		std::cout << "\n==============================" << std::endl;
+		std::cout << cnt + 1 << ". " << "계좌ID : " << GetId() << std::endl;
+		std::cout << "  고객 성함 : " << GetName() << std::endl;
+		std::cout << "  고객 잔고 : " << GetReserve() << std::endl;
+		std::cout << "고객 이자율 : " << GetInterest() << std::endl;
+		++cnt;
+	}
+
+	double HCACustomer::ShowInterestRate() const {
+		return NACustomer::ShowInterestRate() + (creditrate)/(double)100;
+	}
+
+	int HCACustomer::GetInterest() const {
+		return creditrate + NACustomer::GetInterest();
+	}
+
+	void HCACustomer::ShowAllInfo(int* cnt) const {
+		std::cout << "\n==============================" << std::endl;
+		std::cout << cnt + 1 << ". " << "계좌ID : " << GetId() << std::endl;
+		std::cout << "  고객 성함 : " << GetName() << std::endl;
+		std::cout << "  고객 잔고 : " << GetReserve() << std::endl;
+		std::cout << "고객 이자율 : " << GetInterest() << std::endl;
+		++*cnt;
+	}
 }
+
+
