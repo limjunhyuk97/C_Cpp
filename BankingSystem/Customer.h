3c4a8510b5b31d26aa3031bc8bf84c04c49512ca
Jun Hyuk Lim <59442344+limjunhyuk97@users.noreply.github.com>
2021-01-05 23:39:19 +0900

    Update Customer.h

--- BankingSystem/Customer.h
+++ BankingSystem/Customer.h
@@ -1,6 +1,13 @@
 #include <string>
 #pragma once
 namespace CUSTOMER_INFO {
+
+	namespace CREDIT_LEVEL{
+		enum {
+			LEVEL_A = 1, LEVEL_B =2, LEVEL_C = 3
+		};
+	}
+
 	class Customer {
 	private:
 		int id = 0;
@@ -12,10 +19,35 @@ namespace CUSTOMER_INFO {
 		Customer(const Customer& ref);
 		bool IdCheck(int input) const;
 		void ReserveIn(int in);
+		void ReserveInterest();
 		void ReserveOut(int out);
-		int IdReturn() const;
-		int ReserveReturn() const;
-		std::string NameReturn() const;
+		int GetId() const;
+		int GetReserve() const;
+		std::string GetName() const;
+		virtual double ShowInterestRate() const = 0;
+		virtual int GetInterest() const = 0;
+		virtual void ShowAllInfo(int *cnt) const = 0;
 	};
 
+	class NACustomer : public Customer {
+	private:
+		int interest;
+	public:
+		NACustomer(int id, std::string name, int reserve, int interest)
+			:Customer(id, name, reserve), interest(interest) {}
+		double ShowInterestRate() const;
+		int GetInterest() const;
+		void ShowAllInfo(int *cnt) const;
+	};
+
+	class HCACustomer : public NACustomer {
+	private:
+		int creditrate;
+	public:
+		HCACustomer(int id, std::string name, int reserve, int interest, int creditrate)
+			:NACustomer(id, name, reserve, interest), creditrate(creditrate) {}
+		double ShowInterestRate() const;
+		int GetInterest() const;
+		void ShowAllInfo(int *cnt) const;
+	};
 }
