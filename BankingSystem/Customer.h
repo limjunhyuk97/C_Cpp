b1b485d41ad6090ec01d71113dc41eb50a27fc11
Jun Hyuk Lim <59442344+limjunhyuk97@users.noreply.github.com>
2021-01-06 10:52:52 +0900

    Update Customer.h

--- BankingSystem/Customer.h
+++ BankingSystem/Customer.h
@@ -26,7 +26,6 @@ namespace CUSTOMER_INFO {
 		std::string GetName() const;
 		virtual double ShowInterestRate() const = 0;
 		virtual int GetInterest() const = 0;
-		virtual void ShowAllInfo(int *cnt) const = 0;
 	};
 
 	class NACustomer : public Customer {
@@ -37,17 +36,31 @@ namespace CUSTOMER_INFO {
 			:Customer(id, name, reserve), interest(interest) {}
 		double ShowInterestRate() const;
 		int GetInterest() const;
-		void ShowAllInfo(int *cnt) const;
 	};
 
 	class HCACustomer : public NACustomer {
 	private:
 		int creditrate;
+		int additionalinterest;
 	public:
 		HCACustomer(int id, std::string name, int reserve, int interest, int creditrate)
-			:NACustomer(id, name, reserve, interest), creditrate(creditrate) {}
+			:NACustomer(id, name, reserve, interest), creditrate(creditrate) {
+			switch (creditrate) {
+			case 1:
+				additionalinterest = 7;
+				return;
+			case 2:
+				additionalinterest = 4;
+				return;
+			case 3:
+				additionalinterest = 2;
+				return;
+			default:
+				additionalinterest = 0;
+				return;
+			}
+		}
 		double ShowInterestRate() const;
 		int GetInterest() const;
-		void ShowAllInfo(int *cnt) const;
 	};
 }
