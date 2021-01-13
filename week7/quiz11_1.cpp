0cd381417d941e25d725d22403ad113ca5aaf094
Jun Hyuk Lim <59442344+limjunhyuk97@users.noreply.github.com>
2021-01-13 17:00:47 +0900

    Update quiz11_1.cpp

--- week7/quiz11_1.cpp
+++ week7/quiz11_1.cpp
@@ -35,8 +35,8 @@ public:
 		// 1. ref.pistol : class Police 바탕의 ref객체의 , 멤버변수인 Gun 바탕의 pistol 객체 포인터
 		// 2. *(ref.pistol) : class Police 바탕의 ref객체의 , 멤버변수인 Gun 바탕의 pistol 객체 포인터가 가리키는 Gun 객체
 		// 3. new Gun(*(ref.pistol)) : Gun( Gun 객체 )는 복사생성자 호출이다. 결국, ref객체 안의 Gun 객체의 포인터를 new를 통해서 pistol 객체로 할당함.
-    // 결론. 굳이, Gun 객체에 GetBullet 같은 함수 추가하지 않아도 된다. 복사생성자의 원리를 차분하게 생각해보자~
-    // 예시. AAA *aObj = new AAA(bObj); 과 비슷한 상황 aObj에 bObj와 같은 객체 들어가게 됨.
+   		// 결론. 굳이, Gun 객체에 GetBullet 같은 함수 추가하지 않아도 된다. 복사생성자의 원리를 차분하게 생각해보자~
+    		// 예시. AAA *aObj = new AAA(bObj); 과 비슷한 상황 aObj에 bObj와 같은 객체 들어가게 됨.
 	}
 
 	// 대입 연산자 정의
